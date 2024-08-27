73281f7aacb16e61c6a8a72cdcf2eaf120107f62
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-08-27 15:39:55 +0330

    add entities core

---
+++ entity/game.go
@@ -0,0 +1,27 @@
+package entity
+
+import "time"
+
+type Game struct {
+	ID          uint
+	CategoryID  uint
+	QuestionIDs []uint
+	PlayerIDs   []uint
+	StartTime   time.Time
+}
+
+// player is user who is playing in specified game (determine by ID)
+type Player struct {
+	ID      uint
+	UserID  uint
+	GameID  uint
+	Score   uint
+	Answers []PlayerAnswer
+}
+
+type PlayerAnswer struct {
+	ID         uint
+	PlayerID   uint
+	QuestionID uint
+	Choice     PossibleAnswerChoice
+}
