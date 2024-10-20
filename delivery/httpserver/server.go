2aa7fe9dfe1e0f37173c760078a3b6b5d3b12102
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-10-20 22:50:37 +0330

    add : singleton logger

--- delivery/httpserver/server.go
+++ delivery/httpserver/server.go
@@ -10,12 +10,14 @@ import (
 	"gapp/service/authservice"
 	"gapp/service/backofficeuserservice"
 	"gapp/service/matchingservice"
+	"gapp/service/presenceservice"
 	"gapp/service/userservice"
 	"gapp/validator/matchingvalidator"
 	"gapp/validator/uservalidator"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.uber.org/zap"
 )
 
 type Server struct {
@@ -32,19 +34,60 @@ func New(config config.Config, authSvc authservice.Service, userSvc userservice.
 	matchingSvc matchingservice.Service,
 	matchingValidator matchingvalidator.Validator,
 	presenceSvc presenceservice.Service) Server {
-
 	return Server{
-		Router:          echo.New(),
-		config:          config,
-		userHandler:     userhandler.New(config.Auth, authSvc, userSvc, userValidator, presenceSvc),
-		matchingHandler: matchinghandler.New(config.Auth, authSvc, matchingSvc, matchingValidator, presenceSvc),
+		Router:                echo.New(),
+		config:                config,
+		userHandler:           userhandler.New(config.Auth, authSvc, userSvc, userValidator, presenceSvc),
+		backofficeUserHandler: backofficeuserhandler.New(config.Auth, authSvc, backofficeUserSvc, authorizationSvc),
+		matchingHandler:       matchinghandler.New(config.Auth, authSvc, matchingSvc, matchingValidator, presenceSvc),
 	}
 }
 
 func (s Server) Serve() {
+	// Middleware
 
-	s.Router.Use(middleware.Logger())
+	s.Router.Use(middleware.RequestID())
+
+	s.Router.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:           true,
+		LogStatus:        true,
+		LogHost:          true,
+		LogRemoteIP:      true,
+		LogRequestID:     true,
+		LogMethod:        true,
+		LogContentLength: true,
+		LogResponseSize:  true,
+		LogLatency:       true,
+		LogError:         true,
+		LogProtocol:      true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			errMsg := ""
+			if v.Error != nil {
+				errMsg = v.Error.Error()
+			}
+
+			logger.Logger.Named("http-server").Info("request",
+				zap.String("request_id", v.RequestID),
+				zap.String("host", v.Host),
+				zap.String("content-length", v.ContentLength),
+				zap.String("protocol", v.Protocol),
+				zap.String("method", v.Method),
+				zap.Duration("latency", v.Latency),
+				zap.String("error", errMsg),
+				zap.String("remote_ip", v.RemoteIP),
+				zap.Int64("response_size", v.ResponseSize),
+				zap.String("uri", v.URI),
+				zap.Int("status", v.Status),
+			)
+
+			return nil
+		},
+	}))
+
+	//s.Router.Use(middleware.Logger())
+
 	s.Router.Use(middleware.Recover())
+
 	// Routes
 	s.Router.GET("/health-check", s.healthCheck)
 
