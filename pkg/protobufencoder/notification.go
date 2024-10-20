1ab36ed032bdfd0058666a96a69f57363bdccfda
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-10-20 22:44:54 +0330

    add: websocket notification serialized by protobuf format

---
+++ pkg/protobufencoder/notification.go
@@ -0,0 +1,47 @@
+package protobufencoder
+
+import (
+	"encoding/base64"
+	"gapp/contract/goproto/notification"
+	"gapp/entity"
+
+	"google.golang.org/protobuf/proto"
+)
+
+func EncodeNotification(mu entity.Notification) string {
+	pbMu := notification.Notification{
+		Type:    mu.EventType,
+		Payload: mu.Payload,
+	}
+
+	payload, err := proto.Marshal(&pbMu)
+	if err != nil {
+		// TODO - log error
+		// TODO - update metrics
+		return ""
+	}
+
+	return base64.StdEncoding.EncodeToString(payload)
+}
+
+func DecodeNotification(data string) entity.Notification {
+	payload, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		// TODO - log
+		// TODO - update metrics
+		return entity.Notification{}
+	}
+
+	pbMu := notification.Notification{}
+	if err := proto.Unmarshal(payload, &pbMu); err != nil {
+		// TODO - log
+		// TODO - update metrics
+		return entity.Notification{}
+	}
+
+	return entity.Notification{
+		EventType: pbMu.Type,
+		Payload:   pbMu.Payload,
+	}
+
+}
